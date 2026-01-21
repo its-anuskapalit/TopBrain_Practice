bfb9d8301aef7cdedd6cbb0ae1a2058a6495928a
Anuska Palit <anuskapalit2004@gmail.com>
2026-01-21 19:48:56 +0530

    new added

---
+++ delegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+delegate void MyDelegate();
+
+class Program
+{
+    static void Show()
+    {
+        Console.WriteLine("Single Delegate Method");
+    }
+    static void Show2()
+    {
+        Console.WriteLine("multiple Delegate Method");
+    }
+
+
+    static void Main()
+    {
+        MyDelegate d = Show;
+        d+= Show2;
+        d();
+    }
+}
